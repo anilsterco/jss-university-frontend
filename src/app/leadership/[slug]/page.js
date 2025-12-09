b96eb4eb1081a82e2570a9b7c4de79dc5e2d0a47
ajayanandSterco <ajay.anand@stercodigitex.com>
2025-12-09 18:13:22 +0530

    Responsive

--- src/app/leadership/[slug]/page.js
+++ src/app/leadership/[slug]/page.js
@@ -7,46 +7,6 @@ import "@/styles/style.css";
 import "@/styles/custom.style.css";
 
 
-// Mock data – Replace with API data later
-// const leadersData = [
-//   {
-//     id: 1,
-//     slug: "jagadguru-sri-shivarathri",
-//     name: "JAGADGURU SRI SHIVARATHRI",
-//     spanName: "DESHIKENDRA MAHASWAMIJI",
-//     designation: "Chancellor",
-//     topImage: "/images/custom-page/about/jagadguru.png",
-//     bioImage: "/images/custom-page/about/leaderhip-placeholder.png",
-//     messageImage: "/images/custom-page/about/chancellor-message.png",
-//     quoteIcon: "/images/custom-page/about/quote.png",
-//     description: {
-//       intro:
-//         "His Holiness Jagadguru Sri Shivarathri Deshikendra Mahaswamiji assumed leadership in 1986 after spiritual training under his illustrious predecessor, HH Jagadguru Dr. Sri Shivarathri Rajendra Mahaswamiji, founder of JSS Mahavidyapeetha.",
-//       paragraph:
-//         "As Chancellor of JSS University, Noida, His Holiness blends spiritual wisdom, inclusive leadership, and a global vision to guide the University’s mission and growth. Under his stewardship, JSS University, Noida, upholds the values of academic excellence, service to society, and innovation in education.",
-//       biography:
-//         "At the young age of 29, His Holiness assumed leadership of the Math and has since extended its presence across India and abroad, establishing educational institutions in Noida (Uttar Pradesh), as well as cultural and spiritual centres in New Delhi, USA, Mauritius, and Dubai. His work addresses challenges in literacy, healthcare, employment generation, and value-building within society, earning recognition and respect worldwide.",
-//     },
-//     message: {
-//       heading: "Message from the Chancellor",
-//       subheading:
-//         "We are happy to state that all members of JSS University, Noida are committed to building a vibrant future for this institution.",
-//       paragraphs: [
-//         "Guided by an excellent strategic plan and the legacy of success provided by JSS Mahavidyapeetha, the University is dedicated to combining academic excellence with an unwavering commitment to innovation, quality, and service.",
-//         "JSS University, Noida, is the result of concerted efforts to re-orient tertiary education — particularly technical education — under the aegis of JSS Mahavidyapeetha. Founded as the educational arm of Sri Suttur Math, the Mahavidyapeetha has, for decades, embraced the value of Education for All.",
-//         "The University’s genesis coincided with the Mahavidyapeetha’s large-scale socio-economic mission marking the centenary celebrations of HH Jagadguru Sri Dr. Shivarathri Rajendra Mahaswamiji. We are confident that JSS University, Noida, will redefine the purpose of technical education and leave a significant mark on India’s educational landscape.",
-//       ],
-//       footer: {
-//         quote:
-//           "This vision can only be realised through the proactive participation of all stakeholders. True success is built on collective effort, and together we can align with the nation’s vision for innovation, skills, and research. We welcome all to join us in transforming education — not just to teach how to make a living, but how to make a life. Education is the soul of society, and it is our moral duty to use it to transmit the heritage of civilization to future generations. This Academy and its legacy of greatness belong to them, and it is our responsibility to pass it forward.",
-//         name: "Jagadguru Sri Shivarathri Deshikendra Mahaswamiji",
-//         designation: "Chancellor, JSS University, Noida",
-//       },
-//     },
-//   },
-// ];
-
-
 const BASE_URL = "https://project-demo.in/jss/api"
 
 export default function LeadershipDetailsPage({ params }) {
@@ -129,7 +89,7 @@ export default function LeadershipDetailsPage({ params }) {
                     alt={sections.banners.name}
                     width={1200}
                     height={600}
-                    style={{ width: "100%", height: "auto", marginTop: "-98px" }}
+                    // style={{ width: "100%", height: "auto", marginTop: "-98px" }}
                     className="img-fluid w-100"
                     
                   />
@@ -142,7 +102,15 @@ export default function LeadershipDetailsPage({ params }) {
                   </figcaption>
                 </figure>
               </div>
-            </div>
+                       <div className="leadership_two_cnt2">
+                    {/* {sections.about.description.map((desc, index) => (
+                      <p key={index}>{desc[0]}</p>
+                    ))} */}
+                   <h3>{sections.banners.name}</h3>
+                      <h5>{sections.banners.designation}</h5>
+                      <p>{sections.banners.short_description}</p>
+                  </div>
+            </div> 
           </div>
         </div>
       </section>
@@ -164,14 +132,16 @@ export default function LeadershipDetailsPage({ params }) {
                 </div>
                 <div className="col-lg-6">
                   <div className="leadership_two_img">
+                    <figure>
                     <Image
                       src={sections.about.image}
                       alt={sections.banners.name}
                       width={600}
                       height={600}
-                      style={{ width: "100%", height: "300px" }}
+                     
                       className="img-fluid"
                     />
+                    </figure>
                   </div>
                 </div>
               </div>
@@ -188,13 +158,15 @@ export default function LeadershipDetailsPage({ params }) {
       <section className="message_section">
         <div className="container">
           <div className="message_warpper">
+            <figure>
             <Image
               src={"/images/custom-page/about/quote.png"}
               alt="Quote Icon"
-              width={600}
-              height={400}
-              style={{ width: 170, height: "auto" , position: "absolute", top: "-50px", zIndex: 0 }}
+              width={148}
+              height={100}
+              // style={{ width: 170, height: "auto" , position: "absolute", top: "-50px", zIndex: 0 }}
             />
+            </figure>
             <div className="row">
               <div className="col-lg-6">
                 <div className="message_text">
@@ -207,13 +179,15 @@ export default function LeadershipDetailsPage({ params }) {
               </div>
               <div className="col-lg-6">
                 <div className="message_img">
+                  <figure>
                   <Image
                     src={sections.message_from_chancellor.video || "/images/custom-page/about/chancellor-message.png"}
                     alt={sections.message_from_chancellor.name}
-                    width={600}
-                    height={400}
-                    style={{ width: "100%", height: "auto" }}
+                    width={500}
+                    height={500}
+                    // style={{ width: "100%", height: "auto" }}
                   />
+                  </figure>
                 </div>
               </div>
             </div>
