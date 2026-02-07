df728f8967b33eccb33f1405f68a82155fece094
gauravsingh <gauravsingh@stercodigitex.com>
2026-02-07 16:17:31 +0530

    about page design changes done

--- src/component/sections/AboutFour.js
+++ src/component/sections/AboutFour.js
@@ -38,10 +38,10 @@ export default function AboutFour({ data }) {
               {section.items
                 ?.sort((a, b) => a.position - b.position)
                 .map((item, idx) => (
-                  <div key={idx} className="row align-items-center mb-5">
+                  <div key={idx} className="about_f_value">
                     {item.image && (
                       <div
-                        className="col-lg-6"
+                        className="ab_fo_imgsec"
                         data-aos="fade-left"
                         data-aos-delay="300"
                       >
@@ -53,8 +53,8 @@ export default function AboutFour({ data }) {
                                 ? item.title.slice(0, 50)
                                 : "About Section"
                             }
-                            width={700}
-                            height={500}
+                            width={600}
+                            height={400}
                             style={{
                               width: "100%",
                               height: "auto",
@@ -64,7 +64,7 @@ export default function AboutFour({ data }) {
                       </div>
                     )}
                     <div
-                      className="col-lg-6"
+                      className="ab_f_content"
                       data-aos="fade-right"
                       data-aos-delay="200">
                       {item.title && <h3>{item.title}</h3>}
