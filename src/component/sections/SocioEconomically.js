0bce50b8e85c3d0bd29c009975f40b762d5be705
jaiGautam <jai14.stercodigitex@gmail.com>
2026-02-13 17:48:03 +0530

    page dynamic

--- src/component/sections/SocioEconomically.js
+++ src/component/sections/SocioEconomically.js
@@ -20,8 +20,6 @@ export default function FacilityOne({ data }) {
           <div className="containerMD">
             <section className="sedg_section_wrap">
               <div className="sedg_row_block border_bottom">
-
-                {/* Image Column */}
                 <div className="sedg_img_col">
                   {item.image && (
                     <figure className="shine-effect">
@@ -37,19 +35,15 @@ export default function FacilityOne({ data }) {
                     </figure>
                   )}
                 </div>
-
-                {/* Content Column */}
                 <div className="sedg_content_col">
                   {item.title && (
                     <h3 className="sedg_title">{item.title}</h3>
                   )}
-
                   {item.desc?.length > 0 &&
                     item.desc.map((para, i) => (
                       <p key={i}>{para.desc}</p>
                     ))}
                 </div>
-
               </div>
             </section>
           </div>
