6acbfdd5d87687d8a20d7b4a2ceb9a2165de36df
ajayanandSterco <ajay.anand@stercodigitex.com>
2026-03-02 17:10:41 +0530

    fix department header

--- src/component/department-components/departmentHeader/DepartmentHeader.js
+++ src/component/department-components/departmentHeader/DepartmentHeader.js
@@ -5,10 +5,10 @@ import Link from "next/link";
 import { usePathname } from "next/navigation";
 import { IoChevronDownOutline } from "react-icons/io5";
 import styles from "./DepartmentHeader.module.css";
-import '@/styles/custom.style.css'
+import "@/styles/custom.style.css";
 
 export default function DepartmentHeader({ className, data }) {
-  const [activeSection, setActiveSection] = useState(data[0].slug);
+  const [activeSection, setActiveSection] = useState(data?.[0].slug);
   const [engineeringDropdown, setEngineeringDropdown] = useState(false);
   const [selectedSchool, setSelectedSchool] = useState(0);
   const [selectedSchoolName, setSelectedSchoolName] = useState("");
@@ -18,10 +18,10 @@ export default function DepartmentHeader({ className, data }) {
   const [hoveredDepartments, setHoveredDepartments] = useState([]);
   const engineeringRef = useRef(null);
   const pathname = usePathname();
-  const currentPage = pathname.split('/')[1];
-  const currentProgram = pathname.split('/')[3];
+  const currentPage = pathname.split("/")[1];
+  const currentProgram = pathname.split("/")[3];
 
-  console.log('currentProgram', pathname);
+  console.log("currentProgram", pathname);
 
   /* ================= Fetch Data ================= */
 
@@ -48,7 +48,6 @@ export default function DepartmentHeader({ className, data }) {
     fetchSchools();
   }, []);
 
-
   useEffect(() => {
     const handleClickOutside = (e) => {
       if (
@@ -60,10 +59,8 @@ export default function DepartmentHeader({ className, data }) {
     };
 
     document.addEventListener("mousedown", handleClickOutside);
-    return () =>
-      document.removeEventListener("mousedown", handleClickOutside);
+    return () => document.removeEventListener("mousedown", handleClickOutside);
   }, []);
-
 
   // useEffect(() => {
   //   const handleScroll = () => {
@@ -92,13 +89,11 @@ export default function DepartmentHeader({ className, data }) {
   //   }
   // };
 
-
   return (
     <div className={`${styles.departmentHeaderWrapper} ${className}`}>
       <div className={styles.stickyHeader}>
         <nav className="containerXl">
           <div className={styles.departmentMenu}>
-
             <div className={styles.schoolDeptWrapper}>
               <div
                 className={styles.schoolToggle}
@@ -106,7 +101,9 @@ export default function DepartmentHeader({ className, data }) {
               >
                 <span>Departments Of</span>
                 <span>
-                  <span className={styles.selectedName}>{selectedDepartmentName || selectedSchoolName}</span>
+                  <span className={styles.selectedName}>
+                    {selectedDepartmentName || selectedSchoolName}
+                  </span>
                   <IoChevronDownOutline />
                 </span>
               </div>
@@ -117,20 +114,21 @@ export default function DepartmentHeader({ className, data }) {
                   ref={engineeringRef}
                 >
                   <div className={styles.engineeringDropdown}>
-
                     <div className={styles.schoolsList}>
                       <h6>Schools</h6>
                       {engineeringData.map((school, idx) => (
                         <Link
                           key={idx}
-                          href={`/department/${school.slug}`}
-                          className={`${styles.schoolItem} ${selectedSchool === idx ? styles.active : ""
-                            }`}
+                          href={`/schools/${school.slug}`}
+                          className={`${styles.schoolItem} ${
+                            selectedSchool === idx ? styles.active : ""
+                          }`}
                           onMouseOver={() => {
                             setHoveredSchool(idx);
                             setHoveredDepartments(school.departments || []);
                           }}
-                          onMouseOut={() => { }}>
+                          onMouseOut={() => {}}
+                        >
                           {school.name}
                         </Link>
                       ))}
@@ -145,7 +143,9 @@ export default function DepartmentHeader({ className, data }) {
                             href={`/department/${dept.slug}`}
                             className={styles.departmentLink}
                             onClick={() => {
-                              setSelectedSchoolName(engineeringData[hoveredSchool].name);
+                              setSelectedSchoolName(
+                                engineeringData[hoveredSchool].name,
+                              );
                               setSelectedDepartmentName(dept.name);
                               setEngineeringDropdown(false);
                             }}
@@ -154,7 +154,9 @@ export default function DepartmentHeader({ className, data }) {
                           </Link>
                         ))
                       ) : (
-                        <p className={styles.noDept}>No departments available</p>
+                        <p className={styles.noDept}>
+                          No departments available
+                        </p>
                       )}
                     </div>
                   </div>
@@ -166,10 +168,10 @@ export default function DepartmentHeader({ className, data }) {
             {data.map((section) => (
               <Link
                 key={section.title}
-                href={'/' + currentPage + '/' + section.slug}
+                href={"/" + currentPage + "/" + section.slug}
                 className={`${styles.navItem} ${section.slug.includes(currentProgram) ? styles.activeNav : ""}`}
-              // ${activeSection === section.slug ? styles.activeNav : ""}
-              // onClick={() => scrollToSection(section.id)}
+                // ${activeSection === section.slug ? styles.activeNav : ""}
+                // onClick={() => scrollToSection(section.id)}
               >
                 {section.title}
               </Link>
