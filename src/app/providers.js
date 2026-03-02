3fb8be8e382736ec8cd7e8925b8b0ea042ec564e
jaiGautam <jai14.stercodigitex@gmail.com>
2026-03-02 17:07:45 +0530

    work leadership

--- src/app/providers.js
+++ src/app/providers.js
@@ -8,7 +8,7 @@ export default function Providers({ children }) {
       new QueryClient({
         defaultOptions: {
           queries: {
-            staleTime: 5 * 60 * 1000, // 5 minutes
+            staleTime: 5 * 60 * 1000, 
             refetchOnWindowFocus: false,
           },
         },
