2d298b3dc909c699eb6d695a26737447de36e4cd
ashwani kumar <ashwani.kumar@stercodigitex.com>
2026-03-14 14:56:35 +0530

    responsive css

--- src/component/Header.js
+++ src/component/Header.js
@@ -2997,6 +2997,8 @@ export default function Header() {
           .menu-list li {
             text-align: center;
             padding-block: 2rem 0;
+            padding-bottom: 0;
+
           }
           .menu-item.active {
             background: #e5ffff;
@@ -3014,6 +3016,7 @@ export default function Header() {
             font-family: var(--font-Roboto);
             letter-spacing: 0px;
             font-weight: 300;
+            margin-bottom: 1rem;
           }
           .menu-list button {
             background: none;
@@ -3025,6 +3028,7 @@ export default function Header() {
           }
           .icon {
             margin-bottom: 1.2rem;
+            height: 2rem;
           }
           .site-header.no-shadow {
             background: none !important;
@@ -3129,6 +3133,14 @@ export default function Header() {
           .innerPage.header-scrolled {
             background: #deebf4;
           }
+            @media (max-width:360px){
+            .header-inner {
+            padding-block: 1.4rem;
+          }
+            .containerXl{ padding-inline:0rem}
+          }
+            .courses-panel::before{    height: 42%;}
+           
         `}
       </style>
     </header>
