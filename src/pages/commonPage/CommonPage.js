9470afbbbe5e37bb81ab7b5bfbbe2b9d3ffddcd2
ajayanandSterco <ajay.anand@stercodigitex.com>
2026-03-18 15:31:14 +0530

    add department research editor

--- src/pages/commonPage/CommonPage.js
+++ src/pages/commonPage/CommonPage.js
@@ -63,6 +63,7 @@ import FacilityTab from "@/component/sections/FacilityTab";
 import StudentClub from "@/component/sections/StudentClub";
 import AmenitiesList from "@/component/sections/AmenitiesList";
 import Accordions from "@/component/sections/Accordions";
+import DepartmentResearch from "@/component/sections/DepartmentResearch";
 
 export default function CommonPage({ data }) {
   const groupedSections = [];
@@ -156,11 +157,12 @@ export default function CommonPage({ data }) {
     tableContent: TableContent,
     tabsContents: TabsContents,
     ResearchPatents: ResearchPatents,
-        facilitiesTab: FacilityTab,
-        studentClub: StudentClub,
-        amenities_list: AmenitiesList,
+    facilitiesTab: FacilityTab,
+    studentClub: StudentClub,
+    amenities_list: AmenitiesList,
     labCard: LabCard,
-        table_section_tabs: Accordions,
+    table_section_tabs: Accordions,
+    departmentResearch: DepartmentResearch,
   };
 
   return (
