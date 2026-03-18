f507a2225d562a89b9942660e4e22941688da218
ajayanandSterco <ajay.anand@stercodigitex.com>
2026-03-18 14:42:39 +0530

    fix table order

--- src/component/common/accordionTable/AccordionTable.js
+++ src/component/common/accordionTable/AccordionTable.js
@@ -1,9 +1,17 @@
 import React from "react";
 
 export default function AccordionTable({ data, heading }) {
+  const sortedData = data
+    ? [...data].sort((a, b) => {
+        const aVal = Number(a?.tableDatas?.[0]?.tableData);
+        const bVal = Number(b?.tableDatas?.[0]?.tableData);
+        if (isNaN(aVal) || isNaN(bVal)) return 0;
+        return aVal - bVal;
+      })
+    : [];
+
   return (
     <>
-      {/* <h5 className="heading">{data?.[0]?.heading}</h5> */}
       <div className="faqList">
         <details className="faqItem">
           <summary className="faqQuestion">
@@ -11,9 +19,9 @@ export default function AccordionTable({ data, heading }) {
             <span className="icon"></span>
           </summary>
           <div className="faqAnswer">
-            <div className={`table_section`}>
-              <div className=" table-responsive">
-                <table className={`table-lab table table-bordered`}>
+            <div className="table_section">
+              <div className="table-responsive">
+                <table className="table-lab table table-bordered">
                   <thead>
                     <tr>
                       {data?.[0]?.tableHeadings?.map((th, thIndex) => (
@@ -22,24 +30,17 @@ export default function AccordionTable({ data, heading }) {
                     </tr>
                   </thead>
                   <tbody>
-                    {data?.length > 0 &&
-                      data.map((singleData, singleIdx) => {
-                        return (
-                          <React.Fragment key={singleIdx}>
-                            {singleData?.tableDatas && (
-                              <tr>
-                                {singleData.tableDatas?.map(
-                                  (tableItem, tableIdx) => (
-                                    <td key={tableIdx}>
-                                      {tableItem.tableData}
-                                    </td>
-                                  ),
-                                )}
-                              </tr>
-                            )}
-                          </React.Fragment>
-                        );
-                      })}
+                    {sortedData.map((singleData, singleIdx) => {
+                      if (!singleData?.tableDatas?.length) return null;
+
+                      return (
+                        <tr key={singleIdx}>
+                          {singleData.tableDatas.map((tableItem, tableIdx) => (
+                            <td key={tableIdx}>{tableItem.tableData} </td>
+                          ))}
+                        </tr>
+                      );
+                    })}
                   </tbody>
                 </table>
               </div>
