8df3725e003837562c29e836c7f84ad8b3c2d7d2
shyam <shyam.saini@stercodigitex.com>
2026-06-12 17:53:17 +0530

    aiml and cse department society css and responsive

--- src/component/sections/TextareaCustom.js
+++ src/component/sections/TextareaCustom.js
@@ -1,8 +1,152 @@
 "use client";
 
-import { useEffect, useState } from "react";
+import { act, useEffect, useRef, useState } from "react";
 import AOS from "aos";
 import "aos/dist/aos.css";
+
+function MobileTab({ item, activeTabs, setActiveTabs }) {
+    const accordionRefs = useRef([]);
+
+    const handleAccordion = (index) => {
+        const isOpening = activeTabs[item.id] !== index;
+
+        setActiveTabs((prev) => ({
+            ...prev,
+            [item.id]: isOpening ? index : null,
+        }));
+
+        if (isOpening) {
+            setTimeout(() => {
+                accordionRefs.current[index]?.scrollIntoView({
+                    behavior: "smooth",
+                    block: "start",
+                });
+            }, 450);
+        }
+    };
+
+    return (
+        <div>
+            <div className="container">
+                {item?.side_menus?.map((menu, index) => (
+                    <div
+                        className="accordion_item"
+                        key={index}
+                        ref={(el) => (accordionRefs.current[index] = el)}
+                    >
+                        <button
+                            className={`accordion_title ${activeTabs[item.id] === index
+                                    ? "active_society"
+                                    : "inactive_society"
+                                }`}
+                            onClick={() => handleAccordion(index)}
+                        >
+                            <span>{menu.name}</span>
+
+                            <svg
+                                className={`accordion_arrow ${activeTabs[item.id] === index
+                                        ? "rotate"
+                                        : ""
+                                    }`}
+                                stroke="currentColor"
+                                fill="currentColor"
+                                viewBox="0 0 512 512"
+                                xmlns="http://www.w3.org/2000/svg"
+                            >
+                                <path
+                                    fill="none"
+                                    strokeLinecap="round"
+                                    strokeLinejoin="round"
+                                    strokeWidth="48"
+                                    d="m112 184 144 144 144-144"
+                                />
+                            </svg>
+                        </button>
+
+                        <div
+                            className={`accordion_content left_side_content ${activeTabs[item.id] === index
+                                    ? "open"
+                                    : ""
+                                }`}
+                        >
+                            <div
+                                dangerouslySetInnerHTML={{
+                                    __html:
+                                        menu.left_overview_data || "",
+                                }}
+                            />
+
+                            {menu.other_data && (
+                                <div
+                                    dangerouslySetInnerHTML={{
+                                        __html: menu.other_data,
+                                    }}
+                                />
+                            )}
+                        </div>
+                    </div>
+                ))}
+            </div>
+        </div>
+    );
+}
+
+function DesktopTab({ item, activeIndex, activeTabs, setActiveTabs }) {
+    return (
+        <div className={''}>
+            <div className="container">
+                <div className={`${item?.side_menus?.length > 1
+                    ? "society_overview_grid"
+                    : "society_overview_center"
+                    }`}>
+                    {item?.side_menus?.length > 1 && (
+                        <div className="right_side_menus">
+                            {item.side_menus.map((menu, index) => (
+                                <div key={index} className="society_links">
+                                    <a
+                                        href="#"
+                                        onClick={(e) => {
+                                            e.preventDefault();
+                                            setActiveTabs(prev => ({
+                                                ...prev,
+                                                [item.id]: index
+                                            }));
+                                        }}
+                                        className={
+                                            activeIndex === index
+                                                ? "active_society"
+                                                : "inactive_society"
+                                        }
+                                    >
+                                        {menu.name}
+                                    </a>
+
+                                </div>
+                            ))}
+                        </div>
+                    )}
+                    <div
+                        className="left_side_content"
+                        dangerouslySetInnerHTML={{
+                            __html:
+                                item?.side_menus?.[activeIndex]
+                                    ?.left_overview_data || ""
+                        }}
+                    />
+                </div>
+            </div>
+            {item?.side_menus?.[activeIndex]?.other_data && (
+                <div
+                    className="all_data"
+                    dangerouslySetInnerHTML={{
+                        __html:
+                            item.side_menus[activeIndex].other_data
+                    }}
+                />
+            )}
+        </div>
+    )
+}
 
 export default function TextareaCustom({ data }) {
     const [activeTabs, setActiveTabs] = useState({});
@@ -10,6 +154,22 @@ export default function TextareaCustom({ data }) {
         AOS.init({ once: true, duration: 1000 });
     }, []);
 
+    const [isMobile, setIsMobile] = useState(
+        window.innerWidth < 991
+    );
+
+    useEffect(() => {
+        const handleResize = () => {
+            setIsMobile(window.innerWidth < 991);
+        };
+
+        window.addEventListener("resize", handleResize);
+
+        return () => {
+            window.removeEventListener("resize", handleResize);
+        };
+    }, []);
+    console.log('isMobile', isMobile);
 
 
     return (
@@ -17,139 +177,28 @@ export default function TextareaCustom({ data }) {
             {data?.map((section, sectionIndex) => {
                 if (section.type !== "textArea_custom") return null;
                 if (!section.items?.length) return null;
-
                 return (
                     <>
                         {section.items.map((item) => {
-
                             const activeIndex = activeTabs[item?.id] ?? 0;
-
                             return (
                                 <div
-                                    className={`department_society_overview mobile_overview ${item?.custom_class || ''}`}
-                                    key={item.id}
+                                    className={`department_society_overview ${item?.custom_class || ''}`}
+                                    key={item?.id}
                                 >
-
-                                    {/* Desktop Layout */}
-                                    <div className="desktop_society_view">
-                                        <div className="container">
-
-                                            <div className={`${item?.side_menus?.length > 1
-                                                ? "society_overview_grid"
-                                                : "society_overview_center"
-                                                }`}>
-
-                                                {item?.side_menus?.length > 1 && (
-                                                    <div className="right_side_menus">
-                                                        {item.side_menus.map((menu, index) => (
-                                                            <div key={index} className="society_links">
-                                                                <a
-                                                                    href="#"
-                                                                    onClick={(e) => {
-                                                                        e.preventDefault();
-
-                                                                        setActiveTabs(prev => ({
-                                                                            ...prev,
-                                                                            [item.id]: index
-                                                                        }));
-                                                                    }}
-                                                                    className={
-                                                                        activeIndex === index
-                                                                            ? "active_society"
-                                                                            : "inactive_society"
-                                                                    }
-                                                                >
-                                                                    {menu.name}
-                                                                </a>
-
-                                                            </div>
-                                                        ))}
-                                                    </div>
-                                                )}
-                                                <div
-                                                    className="left_side_content"
-                                                    dangerouslySetInnerHTML={{
-                                                        __html:
-                                                            item?.side_menus?.[activeIndex]
-                                                                ?.left_overview_data || ""
-                                                    }}
-                                                />
-                                            </div>
-                                        </div>
-                                        {item?.side_menus?.[activeIndex]?.other_data && (
-                                            <div
-                                                className="all_data"
-                                                dangerouslySetInnerHTML={{
-                                                    __html:
-                                                        item.side_menus[activeIndex].other_data
-                                                }}
-                                            />
-                                        )}
-                                    </div>
-
-                                    {/* Mobile Accordion */}
-                                    <div className="mobile_society_view">
-                                        <div className="container">
-                                            {item?.side_menus?.map((menu, index) => (
-                                                <div className="accordion_item" key={index}>
-                                                    <button
-                                                        className={`accordion_title ${activeTabs[item.id] === index
-                                                            ? "active_society"
-                                                            : "inactive_society"
-                                                            }`}
-                                                        onClick={() => {
-                                                            setActiveTabs(prev => ({
-                                                                ...prev,
-                                                                [item.id]:
-                                                                    prev[item.id] === index
-                                                                        ? null
-                                                                        : index
-                                                            }));
-
-                                                        }}
-                                                    >
-                                                        <span>{menu.name}</span>
-
-                                                        <svg
-                                                            className={`accordion_arrow ${activeTabs[item.id] === index ? "rotate" : ""
-                                                                }`}
-                                                            stroke="currentColor"
-                                                            fill="currentColor"
-                                                            viewBox="0 0 512 512"
-                                                            xmlns="http://www.w3.org/2000/svg"
-                                                        >
-                                                            <path
-                                                                fill="none"
-                                                                strokeLinecap="round"
-                                                                strokeLinejoin="round"
-                                                                strokeWidth="48"
-                                                                d="m112 184 144 144 144-144"
-                                                            />
-                                                        </svg>
-                                                    </button>
-
-                                                    <div
-                                                        className={`accordion_content left_side_content ${activeTabs[item.id] === index ? "open" : ""
-                                                            }`}
-                                                    >
-                                                        <div
-                                                            dangerouslySetInnerHTML={{
-                                                                __html: menu.left_overview_data || ""
-                                                            }}
-                                                        />
-
-                                                        {menu.other_data && (
-                                                            <div
-                                                                dangerouslySetInnerHTML={{
-                                                                    __html: menu.other_data
-                                                                }}
-                                                            />
-                                                        )}
-                                                    </div>
-                                                </div>
-                                            ))}
-                                        </div>
-                                    </div>
+                                    {
+                                        isMobile ?
+                                            <>
+                                                {
+                                                    isMobile && item?.side_menus?.length > 1 ?
+                                                        <MobileTab item={item} activeIndex={activeIndex} activeTabs={activeTabs} setActiveTabs={setActiveTabs} />
+                                                        :
+                                                        <DesktopTab item={item} activeIndex={activeIndex} activeTabs={activeTabs} setActiveTabs={setActiveTabs} />
+                                                }
+                                            </>
+                                            :
+                                            <DesktopTab item={item} activeIndex={activeIndex} activeTabs={activeTabs} setActiveTabs={setActiveTabs} />
+                                    }
                                 </div>
                             );
                         })}
